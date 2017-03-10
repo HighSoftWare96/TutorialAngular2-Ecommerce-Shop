2865cccf83d13aae1ae46a14c1f14b9b5564443b
HighSoftWare96 <bertonc96@gmail.com>
2017-03-10 12:20:02 +0100

    Valuta, prodotti e UI principale
    Aggiunti i prodotti di esempio, aggiunta la conversione della valuta,
    strutturata la UI principale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { ShopComponent } from './shop/shop.component';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
-import { ShopComponent } from './shop/shop.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    ShopComponent
+    ShopComponent,
   ],
   imports: [
     BrowserModule,
